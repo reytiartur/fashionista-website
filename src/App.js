fb8b12b99d4214ba2f36e4197673c9ad989528cf
reytiartur <reyti.artur@gmail.com>
2022-09-19 14:15:58 +0200

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+
+import { Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path='/' element={<Navigation />} />
+
+      </Routes>
     </div>
   );
 }
