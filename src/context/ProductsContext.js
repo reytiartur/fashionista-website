40f585a2fc40f53da01db41a38e80cb73aab4584
reytiartur <reyti.artur@gmail.com>
2022-10-05 21:06:12 +0200

    Firestore Database Update Function

--- src/context/ProductsContext.js
+++ src/context/ProductsContext.js
@@ -1,5 +1,6 @@
 import { createContext, useState, useEffect } from "react";
-import { getCategoriesAndDocuments } from "../utils/firebase/firebase";
+import { getCategoriesAndDocuments, addCollectionAndDocuments } from "../utils/firebase/firebase";
+import PRODUCTS from "../products";
 
 export const ProductsContext = createContext({
     products: [],
@@ -11,6 +12,28 @@ export const ProductsContext = createContext({
 export const ProductsProvider = ({children}) => {
     const [products, setProducts] = useState([])
     const [filteredProducts, setFilteredProducts] = useState([])
+
+    //!!! Helper Firestore Database Update Function !!!
+    // useEffect(() => {
+    //   const setCategories = async () => {
+    //     PRODUCTS.forEach(product => product.items ? Object.values(product.items).map(item => {
+    //       const tagArray = []
+    //       for(const sub in item) {
+    //         if(sub !== 'name' && sub !== 'imgUrl' && sub !=='price' && sub !=='slug' && sub !=='tag') { 
+    //           tagArray.push(item[sub])
+    //         }
+    //       }
+    //       item.tag = tagArray.flat();
+    //       return item;
+    //     })
+    //     .map(item => {
+    //         item.slug = (item.name + " " + item.fit).toLowerCase().split(" ").join("-")
+    //         return item
+    //       }) : Object.values(product.title))
+    //     await addCollectionAndDocuments("categories", PRODUCTS)
+    //   }
+    //   setCategories()
+    // }, [])
 
   useEffect(() => {
     const getCategoriesMap = async () => {
