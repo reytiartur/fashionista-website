cc271ac1f4c273b4a9ce9d7c01894e9491321a2f
reytiartur <reyti.artur@gmail.com>
2022-10-09 18:31:44 +0200

    Display filter name button

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,7 +9,7 @@ import CategoriesListMenu from '../CategoriesListMenu/CategoriesListMenu'
 
 const Shop = () => {
   const { products, setProducts, filteredProducts, setFilteredProducts } = useContext(ProductsContext)
-  const { activeFilters, setActiveFilters, filterPrice } = useContext(FilterContext);
+  const { activeFilters, setActiveFilters, filterPrice, checkedValue, setCheckedValue } = useContext(FilterContext);
   const { min, max } = filterPrice
   
 
@@ -20,7 +20,7 @@ const Shop = () => {
   useEffect(() => {
     const filterArray = () => {
       if(activeFilters.length) {
-        const filteredArray = [...products].filter(item => item.tag.some(tag => activeFilters.includes(tag.toLowerCase())))
+        const filteredArray = products.filter(item => item.tag.some(tag => activeFilters.includes(tag.toLowerCase())))
         // const filteredArray = [...products].filter(item => activeFilters.every(filter => item.tag.includes(filter)) ? true : false)   
         setFilteredProducts(filteredArray);
       } else {
@@ -41,11 +41,22 @@ const Shop = () => {
     setFilteredProducts(filteredArray);
   }, [filterPrice])
   
-
+  const handleDeleteFilter = (filter) => {
+    const deleteFilter = activeFilters.filter(item => item !== filter)
+    setCheckedValue({...checkedValue, [filter]: { ...checkedValue[filter], checked: !checkedValue[filter].checked }})
+    setActiveFilters(deleteFilter)
+  }
 
   return (
     <div className='shop-container'>
-      <div className='category-name'>#Category#Name</div>
+      <div className='filters-name'>{activeFilters.map(filter => {
+        return (
+          <div key={filter.toLowerCase()} className='filter-button'>
+            <span >{filter.toLowerCase()}</span>
+            <div onClick={() => handleDeleteFilter(filter)}>&#x2716;</div>
+          </div>
+          )
+      })}</div>
       <CategoriesListMenu />
       <Filter key='filter' />
       <div className="catalog">
